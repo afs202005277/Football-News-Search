8f14261bf6770c500420cfc1391ff141193e2427
Andre Sousa <up202005277@up.pt>
2023-12-04 11:21:08 +0000

    changed port

--- frontend/run.py
+++ frontend/run.py
@@ -4,9 +4,10 @@ import requests
 
 ROWS = 30
 
-BASE_URL = 'http://localhost:8985/solr/news_articles_v3/select' # SCHEMA 3
+BASE_URL = 'http://localhost:8985/solr/news_articles_v3/select'  # SCHEMA 3
 
 app = Flask(__name__)
+
 
 def text_to_embedding(text):
     model = SentenceTransformer('paraphrase-MiniLM-L6-v2')
@@ -18,7 +19,6 @@ def text_to_embedding(text):
 
 
 def solr_knn_query(base_url, embedding):
-
     data = {
         "q": f"{{!knn f=vector topK={ROWS}}}{embedding}",
         "fl": "title,content,date,origin,id",
@@ -34,6 +34,7 @@ def solr_knn_query(base_url, embedding):
     response.raise_for_status()
     return response.json()
 
+
 @app.route('/solr/')
 def solr():
     query = request.query_string.decode('utf-8')
@@ -41,17 +42,18 @@ def solr():
     response = solr_knn_query(BASE_URL, text_to_embedding(query))
     return response['response']['docs']
 
-    '''
+    """
     query = request.query_string.decode('utf-8')
     response = requests.get(f'http://localhost:8983/solr/news_articles/select?{query}')
     response.raise_for_status()
     return response.json()['response']['docs']
-    '''
+    """
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
 
+if __name__ == '__main__':
+    app.run(debug=True, port=8000)
